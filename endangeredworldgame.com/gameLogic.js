c600187b8ea6034072de293cc5356b9ef957e9c6
Liz <liz.montgomery.1991@gmail.com>
2020-04-17 18:46:18 -0600

    added shortcut icon

--- endangeredworldgame.com/gameLogic.js
+++ endangeredworldgame.com/gameLogic.js
@@ -4,9 +4,19 @@ $( function() {
 			return $( this ).prop( 'title' );
 		}
 	});
+	$("#shortcutIcon").tooltip({
+		content: function () {
+			return $( this ).prop( 'title' );
+		},
+		tooltipClass: "custom-tooltip-styling"
+	});
  } );
 
 $('body').on('click', ".help-icon", function(e) {
+	e.preventDefault();
+});
+
+$('body').on('click', "#shortcutIcon", function(e) {
 	e.preventDefault();
 });
 
@@ -33,6 +43,7 @@ $( document ).ready(function() {
 		$("#main").show();
 		$("#main2").css("display", "grid");
 	});
+	
 });
 
 // the Player object will keep track of tokens and points
@@ -89,6 +100,10 @@ function Player(num, name, gold, silver, bronze, plain, defender, bomb) {
   };
 
   this.playBomb = function(tile) {
+	if ($("#playingIcon").is(":visible")) {
+		$("#playBombSound").trigger('play');
+	}
+	
     this.bomb--;
 
     let opponent = Math.abs(num - 1).toString();
@@ -694,9 +709,6 @@ $("#playBomb").click(function() {
 });
 
 function handlePlayBomb() {
-	if ($("#playingIcon").is(":visible")) {
-		$("#playBombSound").trigger('play');
-	}
 	
 	if (currentQuestion == 0) {
 		players[playerNum].playBomb(tileNum);
@@ -1744,10 +1756,6 @@ function placeAnimal(w, h, id, animalType) {
 let bagOfQuestions = [];
 // place the 20 question tiles in free places
 function placeQuestions(id) {
-	// this logic is just too complicated, so I'm removing this question
-	if (id == 12) {
-		return;
-	}
 	
   let placed = false;
 
@@ -1783,6 +1791,18 @@ $("#startGame").click(function() {
   $("#cards").show();
   $("#turnDisplay").html('<p>Click the deck to begin the game!</p>');
   $("#pickNumber").addClass("cardHighlight");
+  
+  
+	let isMobile = false; //initiate as false
+	// device detection
+	if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+		|| /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+		isMobile = true;
+	}
+	
+	if (!isMobile) {
+		$("#shortcutDiv").show();
+	}
 
   bag = new NumberBag();
   bag.fillBag();
@@ -2461,18 +2481,21 @@ function q9(tileNum) {
 		
 		// highlight the relevant spaces
 		let tile;
+		let validSpacesExist = false;
 		for (let i = 0; i < highlighted.length; i++) {
 			// they shouldn't have the user's tile, a question tile, or the comp's defended tile or defender
 			tile = $("#" + highlighted[i]);
 			
 			if (!tile.hasClass("occupied") && !tile.hasClass("question")) {
 				tile.addClass("highlight");
+				validSpacesExist = true;
 			} else if (tile.hasClass("player1") && !tile.hasClass("defendedbyplayer1")) {
 				tile.addClass("highlight");
+				validSpacesExist = true;
 			}
 		}
 		
-		if (highlighted.length > 0) {
+		if (validSpacesExist) {
 			// prompt the user to click on one of these highlighted spaces
 			msg += "<p>Please select the space where you would like to place a token, or click Do Nothing to not place anything.</p>";
 			ruleForClicking = 9;
@@ -2538,7 +2561,7 @@ function q9(tileNum) {
 }
 
 function q10(tileNum) {
-	let qText = "You're feeling adventurous! Move three spaces up or down. If not occupied, you may place any token (including a bomb) in one of these spaces. If occupied, you may place any token in the original space.";
+	let qText = "You're feeling adventurous! Move three spaces up or down. If not occupied, you may place any token (including a bomb) in one of these spaces if not a question tile. If occupied, you may place any token in the original space.";
 	$("#questionCard").text(qText);
 	currentQuestion = 10;
 	
@@ -2711,6 +2734,27 @@ function q11(tileNum) {
 let extraTurnUser = false;
 let extraTurnComp = false;
 function q12(tileNum) {
+	let qText = "Congratulations! You have found a bronze coin. It has been added to your inventory.";
+	$("#questionCard").text(qText);
+	
+	let msg = "";
+	// inform the user what's happening
+	if (playerNum === 0) {
+		msg = "<p>Click below to continue with " + robotName + "'s turn.</p>";
+		players[0].addBronze();
+		$("#continue").html('Continue');
+		$("#continue").show();
+	} else {
+		msg = "<p>Click the deck to continue with your turn.</p>";
+		players[1].addBronze();
+		$("#turnChoices").hide();
+		$("#pickNumber").addClass("cardHighlight");
+	}
+	
+	$("#turnDisplay").html(msg);
+	
+	
+	/*
 	let qText = "All the members of your team have suddenly become ill, so you've lost your next turn.";
 	$("#questionCard").text(qText);
 	
@@ -2747,6 +2791,7 @@ function q12(tileNum) {
 		playerNum++;
 		endHumanTurn();
 	});
+	*/
 
 }
 
@@ -2905,7 +2950,7 @@ function q15(tileNum) {
 }
 
 function q16(tileNum) {
-	let qText = "Volunteers have arrived to help you further your mission. Move three spaces to the right or to the left. If not occupied, you may place any token (including a bomb) in one of these spaces. If occupied, you may place any token in the original space.";
+	let qText = "Volunteers have arrived to help you further your mission. Move three spaces to the right or to the left. If not occupied, you may place any token (including a bomb) in one of these spaces if not a question tile. If occupied, you may place any token in the original space.";
 	$("#questionCard").text(qText);
 	
 	// (see question 10 for similar logic)
